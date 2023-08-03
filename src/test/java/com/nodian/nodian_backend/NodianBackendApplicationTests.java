7a04e4b8473ba3755fffd07b2c2a00c04b7f9164
pvnamGit <pvnam.forwork@gmail.com>
2023-08-03 13:41:40 +0700

    create backend project

---
+++ src/test/java/com/nodian/nodian_backend/NodianBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodian.nodian_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodianBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
